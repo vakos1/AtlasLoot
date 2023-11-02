5a92a03b925707583a48b56b68572d37ccbca586
Lexie <6937574+Lexiebean@users.noreply.github.com>
2023-11-02 13:48:22 +1100

    Kara Trash

--- Locale/locale.en.lua
+++ Locale/locale.en.lua
@@ -36,7 +36,7 @@ AL:RegisterTranslations("enUS", function() return {
 	["ALT+Click to use WishList."] = true,
 	["Left-click to open AtlasLoot.\nMiddle-click for AtlasLoot options.\nRight-click and drag to move this button."] = true,
 	["Various Locations"] = true,
-	["NoticeFrame"] = "If you find anything missing, please report it at |cff3366CChttps://github.com/Lexiebean/AtlasLoot/issues|r or mail |cffbe5effLexie|r.",
+	["NoticeFrame"] = "If you find anything missing, please report it at |cffbe5effhttps://github.com/Lexiebean/AtlasLoot/issues|r or mail |cff5BCEFAL|cffF5A9B8e|cffFFFFFFi|cffF5A9B8x|cff5BCEFAe|r.",
 	["Toggle AL Panel"] = true,
 	[" is safe."] = true,
 	["Server queried for "] = true,
